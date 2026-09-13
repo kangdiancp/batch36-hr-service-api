a47a302bd4ac76de3149dce439128a5c9af07bad
kangdiancp <kangdiancp@gmail.com>
2026-09-13 09:41:55 +0700

    done

--- employees-load-graphql-test-v3.js
+++ employees-load-graphql-test-v3.js
@@ -1,7 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend, Counter } from 'k6/metrics';
-
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3002';
 const GRAPHQL_URL = `${BASE_URL}/graphql`;
 
